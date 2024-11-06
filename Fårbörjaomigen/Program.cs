8cdf4a7c4e932eff0f20ff31b06316b6894b6c23
huca06001 <huca06001@E-PW05C37R>
2024-11-06 11:51:55 +0100

    Add project files.

---
+++ Fårbörjaomigen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace fårbörjaomigen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är snart klar med kapitel 1");
+            Console.WriteLine("Du har lärt dig skriva tecken som\"");
+        }
+    }
+}
